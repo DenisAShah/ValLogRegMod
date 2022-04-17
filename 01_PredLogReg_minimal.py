00d25eb29c3536849a34b8261c64e020247dab6a
Daniele Giardiello <daniele.giardiello1@gmail.com>
2022-04-17 22:37:39 +0200

    Including net benefit calculation (python work in progress)

--- 01_PredLogReg_minimal.py
+++ 01_PredLogReg_minimal.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import math
 import statsmodels.api as smf
+import matplotlib.pyplot as plt
 
 # Get work directory
 # os.getcwd()
@@ -19,7 +20,7 @@ vdata.info() # inspect data as in R str()
 ## Data manipulation ---
 # Development data 
 # Converting categorical variables to dummies
-rdata = pd.get_dummies(data = rdata, columns=["ter_pos", "preafp", "prehcg"])
+rdata = pd.get_dummies(data = rdata, columns = ["ter_pos", "preafp", "prehcg"])
 # Dropping columns not needed
 rdata.drop(["ter_pos_No", "preafp_No", "prehcg_No"], axis = 1, inplace = True)
 print(rdata.head(1))
@@ -39,6 +40,7 @@ X['intercept'] = 1.0
 lrm = smf.GLM(y, X, family = smf.families.Binomial())
 result_lrm = lrm.fit()
 result_lrm.summary()
+
 
 # Save estimated predicted probabilites in the development data
 pred = result_lrm.predict(X)
@@ -120,32 +122,97 @@ df_cal = pd.DataFrame({
 df = df_cal.sort_values(by = ['pred'])
 
 # Calibration plots
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
-ax.plot(df_cal.pred, df_cal.obs, "--", label = "Logistic")
-ax.legend(loc = "best")
+plt.plot(df_cal.pred, df_cal.obs, "--", label = "Logistic")
 plt.show()
+plt.clf()
+plt.cla()
+plt.close('all')
 # NOTE: work in progress
 
 
 # Calibration metrics based on a secondary logistic regression model
 cal_metrics = pd.DataFrame(
   {'ICI' : np.mean(abs(df_cal.obs - df_cal.pred)),
-   'E50' : np.median(abs(df_cal.obs - df_cal.pred)),
+   'E50' : np.median(abs(df_cal.obs - df_cal.pred, 
+                     interpolation = 'midpoint')),
    'E90' : np.quantile(abs(df_cal.obs - df_cal.pred), 
-                       0.9)}, index = [0]
+                       0.9, 
+                       interpolation = 'midpoint')}, 
+  index = [0]
 )
 cal_metrics
 
 # Overall performance measures --------------
 # Brier Score
 from sklearn.metrics import brier_score_loss
-bs_lrm = brier_score_loss(y_val, pred_val)
-bs_lrm
+bs_lrm = brier_score_loss(val_out.y_val, val_out.pred_val)
 
 
-# Scaled Brier Score
+# Scaled brier score
+# Develop null model 
+lrm_null = smf.GLM(val_out.y_val, val_out.intercept, 
+                   family = smf.families.Binomial())                  
+result_lrm_null = lrm_null.fit()
+result_lrm_null.summary()
+result_lrm_null.params[0]
+
+val_out_null = pd.DataFrame(
+  { 
+    'y_val' : vdata["tum_res"],
+    'lp_null' : [result_lrm_null.params[0]] * len(vdata),
+    'pred_null' : np.exp([result_lrm_null.params[0]] * len(vdata)) / (1 + np.exp([result_lrm_null.params[0]] * len(vdata)))
+  }
+)
+bs_lrm_null = brier_score_loss(val_out_null.y_val, 
+                               val_out_null.pred_null)
+
+# Brier score results
+overall_metrics = pd.DataFrame(
+  {'Brier Score' : bs_lrm,
+   'Scaled Brier' : (1 - (bs_lrm/bs_lrm_null))
+   },
+   index = [0]
+)
+
+overall_metrics
 
 # Clinical utility ------
-# NOTE: work in progress
+thresholds = np.arange(0, 1, step = 0.01)
+leng = np.arange(0, len(thresholds), step = 1)
+f_all = np.mean(val_out.y_val)
+NB_all = [0] * len(thresholds)
+NB = [0] * len(thresholds)
+
+for j in leng:
+  NB_all[j] = f_all - (1 - f_all) * (thresholds[j] / (1 - thresholds[j]))
+  tdata = val_out[val_out["pred_val"] > thresholds[j]]
+  TP = tdata.y_val.sum() # or np.sum(tdata["y_val"])
+  FP = (tdata["y_val"] == 0).sum()
+  NB[j]= (TP / len(tdata)) - (FP / len(tdata)) * (thresholds[j] / (1 - thresholds[j]))
+  
+  
+# Create dataframe
+df_dca = pd.DataFrame({
+  'threshold' : thresholds,
+  'NB_all' : NB_all,
+  'NB' : NB
+  }
+)
+
+# Plot decision curves
+plt.plot(df_dca.threshold, df_dca.NB, "-", color = "black")
+plt.plot(df_dca.threshold, df_dca.NB_all, color = "gray")
+plt.xlim([0, 1])
+plt.ylim([-0.05, 0.8])
+plt.axhline(y = 0, color = 'black',  '-.')
+ax.set_xlabel("Threshold")
+ax.set_ylabel("Net Benefit")
+ax.set_title("Decision curve")
+plt.show()
+
+# Next steps: 
+# improve plotting, 
+# add confidence bands of calibration plots
+# bootstrapping to provide confidence intervals of
+## c-statistic, discrimination slope, Brier
 
